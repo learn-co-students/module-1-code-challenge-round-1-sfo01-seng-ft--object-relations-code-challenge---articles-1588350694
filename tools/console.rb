ed3aef33f17ff5bb2bb1e8c63165a6984fd5bac9
Guosa_Edebiri <gedebiri@gmail.com>
2020-05-01 11:14:36 -0700

    No message

--- tools/console.rb
+++ tools/console.rb
@@ -8,8 +8,15 @@ end
 ###  WRITE YOUR TEST CODE HERE ###
 
 
-
-
+author1 = Author.new("Dude")
+author2 = Author.new("Bruh")
+magazine1 = Magazine.new("Dude_Magazine", "Men's Health")
+magazine2 = Magazine.new("Gentleman_Magazine", "Men's Health")
+article1 = Article.new(author1, magazine1, "What's up Dude")
+article2 = Article.new(author1, magazine1, "How's it going Bro")
+article3 = Article.new(author1, magazine1, "You good?")
+article4 = Article.new(author2, magazine1, "Nah")
+article5 = Article.new(author1, magazine2, "Good morning, old chap!")
 
 
 
