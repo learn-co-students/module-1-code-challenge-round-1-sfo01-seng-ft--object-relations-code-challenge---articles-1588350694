b8f266e9969c9873127e3af4cfb2a9736cd93fc7
github username <you@example.com>
2020-05-01 11:20:17 -0700

    CC retake

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,53 @@
+require 'pry'
 class Magazine
   attr_accessor :name, :category
-
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def contributors
+    contributors = []
+    Article.all.each do |article|
+      if article.magazine == self
+        contributors << article.author
+      end
+    end
+    contributors.uniq
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|magazine| magazine.name == name}
+  end
+
+  def article_titles
+    titles = []
+    Article.all.each do |article|
+      if article.magazine == self
+        titles << article.title
+      end
+    end
+    titles.uniq
+  end
+
+  def contributing_authors
+    # select the articles that belongs to this magazine
+    # map down the above selection to a list of authors
+    # OR use #article_count && select to filter out authors with less than 2 articles
+    authors = []
+    my_article = Article.all.select {|article| article.magazine == self}
+    my_article.each do |arti|
+      authors << arti.author 
+    end
+    authors.select {|author| author.count(self) }
+    #binding.pry
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,12 +7,29 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+author1 = Author.new("Khanittha K")
+author2 = Author.new("Thorin Bustamante")
+author3 = Author.new("Mike Griglak")
+author4 = Author.new("Lisa Alabama")
 
+magazine1 = Magazine.new("Hello", "Entertainment")
+magazine2 = Magazine.new("KidsCode", "Children")
+magazine3 = Magazine.new("Wow", "Entertainment")
+magazine4 = Magazine.new("Green Beans", "Children")
+magazine5 = Magazine.new("Cook Book", "Cooking")
+magazine6 = Magazine.new("Sew-On", "Craft")
+magazine7 = Magazine.new("Hello People", "Entertainment")
 
-
-
-
-
+article1 = Article.new(author1, magazine1, "Hello Hollywood")
+article2 = Article.new(author1, magazine2, "I Love Coding")
+article3 = Article.new(author3, magazine3, "Wow LA")
+article4 = Article.new(author2, magazine1, "I hate Green Beans")
+article5 = Article.new(author4, magazine5, "Sweet Home Alabama")
+article6 = Article.new(author4, magazine1, "Love Actually")
+article7 = Article.new(author1, magazine1, "La La Land")
+article8 = Article.new(author4, magazine1, "Popcorn")
+article9 = Article.new(author1, magazine1, "Walk of Shame")
+article10 = Article.new(author4, magazine1, "6D TV")
 
 
 ### DO NOT REMOVE THIS
